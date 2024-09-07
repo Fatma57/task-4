e39b0d7d09274a2739d0b23ce56f768556ddf4de
fatma ali <fatmaali10099@gmail.com>
2024-09-07 19:22:12 +0300

    this is my project

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+void main() {
+  String employeeName = "Nada";
+  String employeePhoneNumber = "01123465387";
+  double employeeGpa = 3.5;
+  double employeeAge = 24;
+  int employeeSalary = 5600;
+  bool employeeFitExam = true;
+  print(
+      "the employee name is $employeeName,the phone number is $employeePhoneNumber, the GPA is $employeeGpa,the age is $employeeAge,the salary is $employeeSalary,and the result of the career was $employeeFitExam ");
+}
